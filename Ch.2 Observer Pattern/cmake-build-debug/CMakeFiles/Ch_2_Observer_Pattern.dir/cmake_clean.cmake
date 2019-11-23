e67054dd162a3c676c8ed2aff3f4a3332da19d17
许明旺 <1140990160@qq.com>
2019-11-23 23:54:35 +0800

    creat Ch.2

---
+++ Ch.2 Observer Pattern/cmake-build-debug/CMakeFiles/Ch_2_Observer_Pattern.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ch_2_Observer_Pattern.dir/main.cpp.o"
+  "Ch_2_Observer_Pattern"
+  "Ch_2_Observer_Pattern.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ch_2_Observer_Pattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Ch.2 Observer Pattern/cmake-build-debug/CMakeFiles/Ch_2_Observer_Pattern.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Ch_2_Observer_Pattern.
+# This may be replaced when dependencies are built.
